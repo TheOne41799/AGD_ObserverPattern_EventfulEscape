b1df423df8f5b8c03847e2179380b90bbaba8325
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2024-12-23 17:25:42 +0530

    Improving UI logic

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class KeyView : MonoBehaviour, IInteractable
 {
-    [SerializeField] GameUIView gameUIView;
     public void Interact()
     {
         int currentKeys = GameService.Instance.GetPlayerController().KeysEquipped;
@@ -10,7 +9,8 @@ public class KeyView : MonoBehaviour, IInteractable
         GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
 
         currentKeys++;
-        gameUIView.UpdateKeyText();
+
+        EventService.Instance.OnKeyPickedUp.InvokeEvent(currentKeys);
 
         gameObject.SetActive(false);
     }
